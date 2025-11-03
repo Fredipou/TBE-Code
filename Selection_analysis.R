c59ac952a31d6248f53b9a51cc8976853569ac01
Fredipou <frederick.st-pierre2@usherbrooke.ca>
2025-11-03 10:48:16 -0500

    Start of slection analysis script

---
+++ Selection_analysis.R
@@ -0,0 +1,7 @@
+### Slection gradient analysis 
+### From Lande (1983) and M. Barbour (xxxx)
+
+## Calculating mean phenotypic fitness for each larval stages (for now)
+
+
+## Calculate relative fitness
