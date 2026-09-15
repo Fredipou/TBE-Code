288c086f55d72283acf410238872815d6dea5d47
Fredipou <frederick.st-pierre2@usherbrooke.ca>
2026-09-15 12:02:20 -0400

    SBW Trophic NEtwork

--- SBW_Trophic_Network.R
+++ SBW_Trophic_Network.R
@@ -2,7 +2,7 @@
 
 source("Data_cleaning.R")
 
-# Cleaning Df and Transforming in long form
+# Cleaning Df and Transforming in long form ----
 
 ## Lepidop and plants
 
@@ -65,7 +65,7 @@ ggraph(layout_lep_ptoid) +
   theme_void() +
   theme(legend.position = "bottom")
 
-### Grouping plants by traits
+### Grouping plants by traits ----
 
 feuillus <- c("Acer saccharum", "Acer saccharinum", "Acer spicatum", 
               "Acer rubrum", "Acer negundo", "Salix sp",
@@ -113,4 +113,167 @@ arbustes <- c("Amelanchier alnifolia", "Amelanchier canadensis",
 herbacees <- c("Aralia nudicaulis", "Solidago sp.",
                "Urtica dioica", "Sanguisorba officinalis",
                "Aster novae-angliae", "Silene vulgaris")
+
+## Creating Shiny with tree host grouped by tree type (coniferous or deciduous) ----
+
+SBW_Trophic_Grouped <- fluidPage(
+  titlePanel("Sous-réseau Parasitoïdes → Hôtes → Plantes"),
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(
+        "parasitoids",
+        "Sélectionner un ou plusieurs parasitoïdes :",
+        choices = sort(unique(edges_lep_ptoid$to)),
+        multiple = TRUE
+      )
+    ),
+    mainPanel(
+      plotOutput("network_plot", height = "750px")
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  output$network_plot <- renderPlot({
+    
+    req(input$parasitoids)
+    
+    edges_lep_ptoid_sub <- edges_lep_ptoid %>%
+      filter(to %in% input$parasitoids) %>%
+      mutate(weight = 1)
+    
+    herbivores_sel <- unique(edges_lep_ptoid_sub$from)
+    
+    edges_pl_lep_sub <- edges_pl_lep %>%
+      filter(to %in% herbivores_sel) %>%
+      mutate(
+        plant_group = case_when(
+          from %in% feuillus   ~ "Feuillus",
+          from %in% coniferes  ~ "Conifères",
+          from %in% arbustes   ~ "Arbustes",
+          from %in% herbacees  ~ "Herbacées",
+          TRUE ~ "Autres plantes"
+        )
+      ) %>%
+      group_by(plant_group, to) %>%
+      summarise(weight = n(), .groups = "drop") %>%
+      rename(from = plant_group)
+    
+    # Combine
+    edges_sub <- bind_rows(edges_pl_lep_sub, edges_lep_ptoid_sub)
+    
+    #  Graph
+    g_sub <- graph_from_data_frame(edges_sub, directed = TRUE)
+    
+    g_tbl_sub <- as_tbl_graph(g_sub) %>%
+      mutate(
+        trophic_level = case_when(
+          name %in% edges_pl_lep_sub$from ~ "Plant",
+          name %in% edges_pl_lep_sub$to ~ "Herbivore",
+          TRUE ~ "Parasitoid"
+        )
+      )
+    
+    layout_sub <- create_layout(g_tbl_sub, layout = "sugiyama")
+    
+    #  Plot
+    
+    ggraph(layout_sub) +
+      geom_edge_link(aes(width = weight), alpha = 0.4) +
+      scale_edge_width(range = c(0.5, 3)) +
+      geom_node_point(aes(color = trophic_level), size = 5) +
+      geom_node_text(aes(label = name), repel = TRUE, size = 3, max.overlaps = 200) +
+      scale_color_manual(values = c(
+        Plant = "forestgreen",
+        Herbivore = "orange",
+        Parasitoid = "red"
+      )) +
+      guides(edge_width = guide_legend(title = "Nb espèces végétales")) +
+      scale_y_reverse() +
+      ggtitle(paste("Sous-réseau pour :", paste(input$parasitoids, collapse = ", "))) +
+      theme_void() +
+      theme(legend.position = "bottom")
+    
+  })
+}
+
+shinyApp(SBW_Trophic_Grouped, server)
+
+## Same thing but with un-grouped tree species ----
+
+SBW_Trophic_by_species <- fluidPage(
+  titlePanel("Sous-réseau Parasitoïdes → Hôtes → Plantes"),
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(
+        "parasitoids",
+        "Sélectionner un ou plusieurs parasitoïdes :",
+        choices = sort(unique(edges_lep_ptoid$to)),
+        multiple = TRUE
+      )
+    ),
+    mainPanel(
+      plotOutput("network_plot", height = "700px")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$network_plot <- renderPlot({
+    
+    req(input$parasitoids)  # ne rien afficher si rien sélectionné
+    
+    # Filtrer interactions Lepidoptères → Parasitoïdes sélectionnés
+    edges_lep_ptoid_sub <- edges_lep_ptoid %>%
+      filter(to %in% input$parasitoids)
+    
+    
+    # Hôtes sélectionnés
+    herbivores_sel <- unique(edges_lep_ptoid_sub$from)
+    
+    # Filtrer interactions Plantes → Lépidoptères pour ces hôtes
+    edges_pl_lep_sub <- edges_pl_lep %>%
+      filter(to %in% herbivores_sel)
+    
+    # Combiner les arêtes
+    edges_sub <- bind_rows(edges_pl_lep_sub, edges_lep_ptoid_sub)
+    
+    # Créer le graphe
+    g_sub <- graph_from_data_frame(edges_sub, directed = TRUE)
+    
+    # Ajouter trophic level
+    g_tbl_sub <- as_tbl_graph(g_sub) %>%
+      mutate(
+        trophic_level = case_when(
+          name %in% edges_pl_lep_sub$from ~ "Plant",
+          name %in% edges_pl_lep_sub$to ~ "Herbivore",
+          TRUE ~ "Parasitoid"
+        ),
+        level = case_when(
+          trophic_level == "Plant" ~ 1,
+          trophic_level == "Herbivore" ~ 2,
+          trophic_level == "Parasitoid" ~ 3
+        )
+      )
+    
+    # Layout
+    layout_sub <- create_layout(g_tbl_sub, layout = "sugiyama")
+    
+    # Graphe
+    ggraph(layout_sub) +
+      geom_edge_link(alpha = 0.3) +
+      geom_node_point(aes(color = trophic_level), size = 4) +
+      geom_node_text(aes(label = name), repel = TRUE, size = 3, max.overlaps = 200) +
+      scale_color_manual(values = c(Plant = "forestgreen", Herbivore = "orange", Parasitoid = "red")) +
+      scale_y_reverse() +
+      ggtitle(paste("Sous-réseau pour :", paste(input$parasitoids, collapse = ", "))) +
+      theme_void() +
+      theme(legend.position = "bottom")
+  })
+}
+
+shinyApp(SBW_Trophic_by_species, server)
 
